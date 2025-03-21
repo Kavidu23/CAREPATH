67ed7fa5700a32a9a35352efbaa52a3a465f44a1
Kavidu23 <10952412@students.plymouth.ac.uk>
2025-03-21 09:16:00 +0530

    front end changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Import the Home component
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },  // Route for Home
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to Home
+  { path: '**', redirectTo: '/home' } // Wildcard route (handles unknown routes)
+];
